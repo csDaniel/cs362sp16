ed9a23ef04ae4118ba840864bd033bbaf25ead40
Brandon Swanson <swanyriver@gmail.com>
2016-05-08 17:07:20 -0700

    adventurer half way through controlling number of coins

--- projects/swansonb/dominion/randomtestadventurer.c
+++ projects/swansonb/dominion/randomtestadventurer.c
@@ -6,53 +6,31 @@
 #include <math.h>
 #include <limits.h>
 
-struct adventurerTestCase {
-    int* deck;
+
+const int RANDOM_MIN_DECK = 6;
+const int RANDOM_MAX_DECK = MAX_DECK - 2;
+const int RANDOM_MAX_HAND = 20;
+
+struct randomDeck {
+    int deck[MAX_DECK];
     int deckSize;
-    int deckSizeAfter;
-    int discardAfter[MAX_DECK];
-    int discardAfterSize;
 };
 
 int isCoin(int card){
     return card == copper || card == silver || card == gold;
 }
 
-int numTreasureCards(int* arr, int n){
-    int num = 0;
-    int i=0;
-    for(;i<n;++i){
-        if (isCoin(arr[i])) num++;
+int numCoinsInPlayersDeckAndDiscard(struct gameState* state, int player ){
+    int i =0;
+    int coinCards = 0;
+    for (;i<state->deckCount[player];++i){
+        coinCards += isCoin(state->deck[player][i]);
     }
-    return num;
-}
-
-/*
-struct adventurerTestCase initTestCase(){
-    struct adventurerTestCase out;
-    out.deck = testDecks + (j*8);
-    out.deckSize = 8;
-    out.deckSizeAfter = 0;
-    out.discardAfterSize = 0;
-
-    int i;
-    int numCoinsFound = 0;
-    for(i=out.deckSize-1; i>=0; --i){
-        if(isCoin(out.deck[i])){
-            numCoinsFound++;
-        } else {
-            out.discardAfter[out.discardAfterSize++] = out.deck[i];
-        }
-        if (numCoinsFound == 2){
-            out.deckSizeAfter = i;
-            break;
-        }
+    for (i=0;i<state->discardCount[player];++i){
+        coinCards += isCoin(state->discard[player][i]);
     }
-
-
-    return out;
+    return coinCards;
 }
-*/
 
 
 void randomizeGame(void *pState) {
@@ -63,6 +41,53 @@ void randomizeGame(void *pState) {
     }
 }
 
+void startTurnForPlayer(struct gameState *pState, int player, int* deck, int arraySize, int numInDeck){
+
+    pState->deckCount[player] = 0;
+    pState->discardCount[player] = 0;
+    pState->handCount[player] = ((int) (Random() * RANDOM_MAX_HAND)) + 1;
+
+    int i=0;
+    for (;i < numInDeck; ++i){
+        pState->deck[player][pState->deckCount[player]++] = deck[i];
+    }
+    for(;i < arraySize; ++i){
+        pState->discard[player][pState->discardCount[player]++] = deck[i];
+    }
+
+    for(i=0; i<pState->handCount[player]; ++i){
+        pState->hand[player][i] = (int) (Random() * treasure_map);
+    }
+
+    pState->whoseTurn = player;
+    pState->phase = 0;
+    pState->numActions = 1;
+    pState->coins = 0;
+    pState->numBuys = 1;
+    pState->playedCardCount = 0;
+
+}
+
+void getRandomDeck(struct randomDeck* deck){
+    deck->deckSize = ((int) (Random() * (RANDOM_MAX_DECK - RANDOM_MIN_DECK + 1))) + RANDOM_MIN_DECK;
+    int numCoins = 1;
+
+    int i = 0;
+    for (;i<deck->deckSize - numCoins;++i){
+
+        //generate random number mapped to all available cards except coins;
+        int randomNumber = (int) (Random() * (treasure_map - 3));
+        if (randomNumber < copper) deck->deck[i] = randomNumber;
+        else deck->deck[i] = randomNumber + adventurer;  //first card after gold
+    }
+
+    for (; i<deck->deckSize; ++i){
+        deck->deck[i] = copper;
+    }
+
+}
+
+
 int main(int argc, char **argv) {
 
     // Testing for playAdventurer function
@@ -72,78 +97,99 @@ int main(int argc, char **argv) {
     printf("--------------------------------------------------------------\n");
     printf("--------------------------------------------------------------\n");
 
-    int k;
-    for ( k = 0; k < 5 ; ++k) {
-        printf("%f \n", Random());
-    }
+
+    const int NUM_DECKS_TO_GENERATE = 20;
 
 
-    const int NUM_DECKS_TO_GENERATE = 5;
-
-
-    int seed = 1000;
     int testsRun = 0;
     int testsPassed = 0;
-    int numPlayers = 2;
     struct gameState G, before;
-    int i,j;
-    int deckIsSame;
+    int i,numInPlayersDeck;
     int curPlayer = 0;
-    /*int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
-               sea_hag, tribute, smithy, council_room};*/
+    struct randomDeck testDeck;
 
-/*    for (i=0; i<NUM_DECKS_TO_GENERATE; ++i){
+    for (i=0; i<NUM_DECKS_TO_GENERATE; ++i){
 
-        struct adventurerTestCase testCase = initTestCase(i, testDecks);
+        //randomize cards available to player
+        getRandomDeck(&testDeck);
 
-        randomizeGame(&G);
-        curPlayer = G.whoseTurn;
+        //exhaustively test deck distributed between players deck and players discard
+        for (numInPlayersDeck=0; numInPlayersDeck<testDeck.deckSize; numInPlayersDeck++) {
+            randomizeGame(&G);
+            curPlayer = (int) (Random() * MAX_PLAYERS);
+            startTurnForPlayer(&G, curPlayer, testDeck.deck, testDeck.deckSize, numInPlayersDeck);
+            curPlayer = G.whoseTurn;
 
-        G.hand[curPlayer][G.handCount[curPlayer]++] = adventurer;
-        copyGameState(&before,&G);
-        playAdventurer(&G, G.handCount[curPlayer]-1);
-        printf("Test that require player to shuffle #:%d playing adventurer card\n",i);
-        printf("Player's hand has 2 new coin cards ");
-        if (G.handCount[curPlayer] == before.handCount[curPlayer]+1 &&
+            G.hand[curPlayer][G.handCount[curPlayer]++] = adventurer;
+            copyGameState(&before,&G);
+            printf("Random Adventurer test #:%d : %d    Coins Available in Player's Deck and Discard:%d\n",
+                   i,numInPlayersDeck, numCoinsInPlayersDeckAndDiscard(&G, curPlayer));
+            playAdventurer(&G, G.handCount[curPlayer]-1);
+
+            printf("Player's hand has 2 new coin cards ");
+            if (G.handCount[curPlayer] == before.handCount[curPlayer]+1 &&
                 isCoin(G.hand[curPlayer][G.handCount[curPlayer]-1]) &&
                 isCoin(G.hand[curPlayer][G.handCount[curPlayer]-2])){
-            testsPassed++;
-            printf("(PASSED) \n");
-        } else {
-            printf("(FAILED) \n");
-        }
-        testsRun += 1;
+                testsPassed++;
+                printf("(PASSED) \n");
+            } else {
+                printf("(FAILED) \n");
+                // Debuging the reason for failed tests
+                // when 1 coin card was available in the players deck/discard the adventure function exited with only
+                // one coin card in the players hand
 
-        printf("Player still has same card in deck/hand/discard ");
-        if (playerHasSameCards(&G,&before,curPlayer)){
-            testsPassed++;
-            printf("(PASSED) \n");
-        } else {
-            printf("(FAILED) \n");
-        }
-        testsRun += 1;
+               /* printf("FAILED:  {");
+                int z = 0;
+                for (;z<G.handCount[curPlayer]; ++z){
+                    printf("%d,",G.hand[curPlayer][z]);
+                }
+                printf("%s","}   {");
+                for (z=0;z<G.handCount[curPlayer]; ++z){
+                    printf("%d,",isCoin(G.hand[curPlayer][z]));
+                }
+                printf("%s","} \n Deck: {");
+                for (z=0;z<testDeck.deckSize; ++z){
+                    printf("%d,",testDeck.deck[z]);
+                }
+                printf("%s","}   {");
+                for (z=0;z<testDeck.deckSize; ++z){
+                    printf("%d,",isCoin(testDeck.deck[z]));
+                }
+                printf("%s", "}\n");*/
+            }
+            testsRun += 1;
 
-        //rollback intentional changes to game state and check for any side-effects
-        G.deckCount[curPlayer] = before.deckCount[curPlayer];
-        G.handCount[curPlayer] = before.handCount[curPlayer];
-        G.discardCount[curPlayer] = before.discardCount[curPlayer];
-        G.playedCardCount = before.playedCardCount;
-        cpyDeck(G.hand[curPlayer], before.hand[curPlayer], MAX_HAND);
-        cpyDeck(G.discard[curPlayer], before.discard[curPlayer], MAX_DECK);
-        cpyDeck(G.deck[curPlayer], before.deck[curPlayer], MAX_DECK);
-        cpyDeck(G.playedCards, before.playedCards, MAX_DECK);
-        printf("The rest of the gameState was unaffected ");
-        if (equalGameStates(&before, &G)){
-            testsPassed++;
-            printf("(PASSED) \n");
-        } else {
-            printf("(FAILED) \n");
+            printf("Player still has same card in deck/hand/discard ");
+            if (playerHasSameCards(&G,&before,curPlayer)){
+                testsPassed++;
+                printf("(PASSED) \n");
+            } else {
+                printf("(FAILED) \n");
+            }
+            testsRun += 1;
+
+            //rollback intentional changes to game state and check for any side-effects
+            G.deckCount[curPlayer] = before.deckCount[curPlayer];
+            G.handCount[curPlayer] = before.handCount[curPlayer];
+            G.discardCount[curPlayer] = before.discardCount[curPlayer];
+            G.playedCardCount = before.playedCardCount;
+            cpyDeck(G.hand[curPlayer], before.hand[curPlayer], MAX_HAND);
+            cpyDeck(G.discard[curPlayer], before.discard[curPlayer], MAX_DECK);
+            cpyDeck(G.deck[curPlayer], before.deck[curPlayer], MAX_DECK);
+            cpyDeck(G.playedCards, before.playedCards, MAX_DECK);
+            printf("The rest of the gameState was unaffected ");
+            if (equalGameStates(&before, &G)){
+                testsPassed++;
+                printf("(PASSED) \n");
+            } else {
+                printf("(FAILED) \n");
+            }
+            testsRun += 1;
         }
-        testsRun += 1;
 
     }
 
-    printf("%d of %d tests passed\n",testsPassed, testsRun);*/
+    printf("%d of %d tests passed\n",testsPassed, testsRun);
 
     return 0;
 }
