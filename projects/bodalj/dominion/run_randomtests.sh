3036e6162e7e35c0f6f8b14efc6736b4c7cf969d
Jens Bodal <bodalj@onid.oregonstate.edu>
2016-05-08 21:08:02 -0700

    fixed output for randomtests

--- projects/bodalj/dominion/run_randomtests.sh
+++ projects/bodalj/dominion/run_randomtests.sh
@@ -19,12 +19,16 @@ echo >> $fout
 >$rtc_out
 make clean
 make randomtestcard
-echo "-- randomtestcard results --" >> $rtc_out
-randomtestcard >> $rtc_out
-echo >> $rtc_out
+randomtestcard &> /dev/null
 echo "-- gcov for function 'playVillage' in randomtestcard --" >> $rtc_out
 gcov -fb dominion.c | grep -A 4 "^Function 'playVillage'" >> $rtc_out
 echo >> $rtc_out
 echo >> $rtc_out
 echo "Total code coverage for all functions in dominion.c" >> $rtc_out
 gcov -fb dominion.c >> $rtc_out
+echo >> $rtc_out
+echo >> $rtc_out
+echo "-- randomtestcard results --" >> $rtc_out
+randomtestcard >> $rtc_out
+echo >> $rtc_out
+
