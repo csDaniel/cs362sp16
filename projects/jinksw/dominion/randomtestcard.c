eba31754e861cee3c651a52b4e431e62ba1567f0
Wesley Jinks <jinksw@flip3.engr.oregonstate.edu>
2016-05-09 00:59:25 -0700

    update

--- projects/jinksw/dominion/randomtestcard.c
+++ projects/jinksw/dominion/randomtestcard.c
@@ -1,7 +1,5 @@
 /*
-Adventurer Card: Reveal/Draw cards from deck until you reveal/draw 
-					two treasure cards. Put the treasure cards in your hand,
-					and discard the other revealed cards.
+Smithy Card: Draw 3 cards 
 
 */
 
@@ -16,65 +14,44 @@ Adventurer Card: Reveal/Draw cards from deck until you reveal/draw
 #define DEBUG 0
 #define NOISY_TEST 1
 
-int checkAdventurer(struct gameState *post) {
+int checkSmithy(struct gameState *post) {
   struct gameState pre;
   memcpy (&pre, post, sizeof(struct gameState));
   int r, i;
 	    
-  r = playAdventurer(post);
+  r = playSmithy(post);
 
 	int treasCount = 0;
 	int currPlayer = whoseTurn( &pre );
 	int cardDrawn;
 	int temphand[MAX_HAND];
 	int k = 0;
-	
-	while( treasCount < 2 ){
+
+	for( i=0; i<3; i++ ){	
 		drawCard( currPlayer, &pre ); //drawCard calls shuffle if empty
-
-		if( pre.hand[currPlayer][pre.handCount[currPlayer]] == copper ){
-			treasCount++;
-		}
-		else if( pre.hand[currPlayer][pre.handCount[currPlayer]] == silver ){
-			treasCount++;
-		}
-		else if( pre.hand[currPlayer][pre.handCount[currPlayer]] == gold ){
-			treasCount++;
-		} else {
-			//Remove non-treasure drawn cards from hand and put into temp hand
-			// to discard after turn
-			temphand[k] = pre.hand[currPlayer][pre.handCount[currPlayer]];
-			pre.handCount[currPlayer]--;
-		}
-	}
-
-	while( k>0 ){
-		pre.discardCount[currPlayer]++;
-		pre.discard[currPlayer][pre.discardCount[currPlayer]] = temphand[k];
-		k--;
 	}
 
   assert (r == 0);
 	if( (memcmp(&pre, post, sizeof(struct gameState)) == 0)){
-		printf( "***Passed /n");
+		printf( "+++++++++++++++++++++++++++++++++++++    Passed +++++++++++/n");
 	} else {
-		printf( "***Failed /n" );
-		printf( "   Discard Count Test: %d, Function: %d \n", pre.discardCount[currPlayer],  post->discardCount[currPlayer] );
-		printf( "   Cards in Discard Test: " );
+		printf( "-------------------------------------    Failed -----------/n" );
+		printf( "   #####  Discard Count Test: %d, Function: %d \n", pre.discardCount[currPlayer],  post->discardCount[currPlayer] );
+		printf( "   #####  Cards in Discard Test: " );
 		printDiscard( currPlayer, &pre );
-		printf( "   Cards in Discard Function: " );
+		printf( "   #####  Cards in Discard Function: " );
 		printDiscard( currPlayer, post );
-		printf( "   Hand Count Test: %d, Function: %d \n", pre.handCount[currPlayer],  post->handCount[currPlayer] );
-		printf( "   Cards in Hand Test: " );
+		printf( "\n    ######  Hand Count Test: %d, Function: %d \n", pre.handCount[currPlayer],  post->handCount[currPlayer] );
+		printf( "   ######   Cards in Hand Test: " );
 		printHand( currPlayer, &pre );
-		printf( "   Cards in Hand Function: " );
+		printf( "   #####   Cards in Hand Function: " );
 		printHand( currPlayer, post );
-		printf( "   Deck Count Test: %d, Function: %d \n", pre.deckCount[currPlayer],  post->deckCount[currPlayer] );
-		printf( "   Cards in Deck Test: " );
+		printf( "\n   ####   Deck Count Test: %d, Function: %d \n", pre.deckCount[currPlayer],  post->deckCount[currPlayer] );
+		printf( "   #####   Cards in Deck Test: " );
 		printDeck( currPlayer, &pre );
-		printf( "   Cards in Deck Function: " );
+		printf( "   #####   Cards in Deck Function: " );
 		printDeck( currPlayer, post );
-		printf( "   Coin Count Test: %d, Function: %d \n ", countHandCoins( currPlayer, &pre),  countHandCoins( currPlayer, post ) );
+		printf( "\n   #####   Coin Count Test: %d, Function: %d \n ", countHandCoins( currPlayer, &pre),  countHandCoins( currPlayer, post ) );
 	}
 	
 	return 0;
@@ -88,7 +65,7 @@ int main () {
 	       remodel, smithy, village, baron, great_hall};
   struct gameState G;
 
-  printf ("Testing Adventurer.\n");
+  printf ("Testing Smithy.\n");
 
 
   SelectStream(2);
@@ -101,7 +78,7 @@ int main () {
     G.deckCount[p] = floor(Random() * MAX_DECK);
     G.discardCount[p] = floor(Random() * MAX_DECK);
     G.handCount[p] = floor(Random() * MAX_HAND);
-	printf( "Test %d \n", n );
+	printf( "**************************   Test %d   ****************************** \n", n );
     checkAdventurer(&G);
   }
 
