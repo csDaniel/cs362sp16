d3d36c02a11a9dcd2890abae82523ea279731d73
Heidi Binder-Vitti <heidi@Heidis-MacBook-Pro.local>
2016-05-09 18:33:12 -0700

    A4 adventurer

---
+++ projects/bindervh/dominion/randomhistory.c
@@ -0,0 +1,4 @@
+randomtestadventurer.c
+
+This test uses a random number of players between 2 and 4 and chooses a random set of cards to be in the deck when adventurer is played.  It tests for the correct number of coins added to the hand and that nothing else is changed. This test covered 100% of the branches and 59% of lines.
+
