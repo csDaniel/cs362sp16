7ff82a7477a0e67d1d50dc8a9dd00d12dec52a06
Heidi Binder-Vitti <heidi@Heidis-MacBook-Pro.local>
2016-05-09 19:01:09 -0700

    A4

--- projects/bindervh/dominion/randomhistory.c
+++ projects/bindervh/dominion/randomhistory.c
@@ -1,4 +1,10 @@
 randomtestadventurer.c
 
-This test uses a random number of players between 2 and 4 and chooses a random set of cards to be in the deck when adventurer is played.  It tests for the correct number of coins added to the hand and that nothing else is changed. This test covered 100% of the branches and 59% of lines.
+This test uses a random number of players between 2 and 4 and chooses a random set of cards to be in the deck when adventurer is played.  
+It tests for the correct number of coins added to the hand and that nothing else is changed. 
+This test covered 100% of the branches and 59% of lines.
 
+randomtestcard.c
+
+This tests the smithy card with a random number of players and random number as the randomSeed to set up the game state.
+The random test has 100% coverage for lines and branches for the playSmithy function.
