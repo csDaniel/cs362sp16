a8e6e82fd3102542711fc8a81a9485ff655a6f98
MikeinRealLife <michaelloo@Michaels-MBP.attlocal.net>
2016-05-22 12:43:42 -0700

    assignment5
    added bugFixes.c

---
+++ projects/loom/dominion/bugFixes.c
@@ -0,0 +1,19 @@
+//
+// Created by Michael Loo on 5/22/16.
+//
+/*
+ * BUGS FOUND
+ *
+ * Michael Loo/loom
+    Village Card - The village cards gives the wrong amount of actions.
+
+    Smithy Card - The player does not draw 3 cards.
+ *
+ *Smithy Card - The player drew 2 cards instead of the expected 3.
+
+Adventurer Card: Player drew non treasure card to hand.
+
+Remodel Card: wrong card added when trading (card action returns -1 on applicable card effect ie returns as non legal move when it is legal)
+ *
+ *
+ */
