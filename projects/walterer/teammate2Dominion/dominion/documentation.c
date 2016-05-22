6a896718b0a46dff1fed9e41a35d73aa536140d2
ejwalters <ejwalters24@gmail.com>
2016-05-22 17:22:14 -0400

    Adding teammate 2

--- projects/walterer/teammate2Dominion/dominion/documentation.c
+++ projects/walterer/teammate2Dominion/dominion/documentation.c
@@ -1,32 +1,23 @@
-/*
- * documentation.c
- *
- *  Created on: April 10, 2016
- *      Author: Brian Brown
- */
+/*CS362 Spring 2016
+Assignment 2
+Tatsiana Clifton
 
+Smithy card: It is an action card. If the player plays the smithy card, it will draw three additional
+cards and place them into the player's hands. Then, the smithy card is discarded from the player's hands.
 
-/* The smithy card, when played, counts as an action for the current turn
- * that allows the current player to add 3 cards to their current hand. After it's effect,
- * the card is then added to the discard pile. It can be purchased for 4 coins.
- *
- * The adventurer card, when played, counts as an action for the current turn
- * that allows the current player to begin revealing cards from the player's deck
- * until two treasure cards have been revealed. Those treasure cards are then added
- * to the player's hand and all other cards, including the adventurer, are added to the
- * user's discard pile. This card costs 6 coins.
- *
- * discardCard() initially checks to see if the card is being validly discarded and not "trashed",
- * (removed from the game). It then calculates how many cards are still in the current player's
- * hand and keeps track of the amount of cards until there are no longer cards present.
- *
- * updateCoins() returns the value of coins that a user has in their hand. When called, the
- * function resets the player's total coins to 0, then adds up the value of the treasure coins
- * in the player's hand (1 for copper, 2 for silver, 3 for gold) and then adds in any bonus coins
- * that may or may not be present. The function then returns an integer value for the amount of coins
- * a user can have for the turn. To me, this indicates the maximum number of coins that a player can
- * play in each turn: the initial hand draw, followed by any actions that increase the amount of
- * coins. Note: nothing in the function removes coins from the total, this must be handled elsewhere,
- * most likely in actions of specific cards.
- */
+Adventurer card: If the player plays the adventurer card, it will draw cards from the player's deck until 
+two treasure cards are drawn. All drawn card that are not a treasure card are kept in a temporary pile
+and are discarded when two treasure cards are found. If during this process the deck becomes empty it 
+is reshuffled.
 
+discardCard(): This function excepts four arguments: handPos, currentPlayer, gameState, trashFlag. 
+It returns zero indicating the success. If trashFlag indicates that a card needs to be discarded,
+the function moves card to played pile, moves card from player's hand decrementing the number 
+of card in the player's hands. If trashFlag indicates that a card needs to be trashed, the function
+removes a card from the game to the trash.
+
+updateCoins(): This function excepts three arguments: player, gameState, and bonus. It returns zero 
+indicating the success. The function goes trough every treasure card in player hands and depending on
+its value calculates the total amount of treasure cards in the player's hands. Then, it adds to 
+this value the amount of the bonus. As a result, number of coins is set to new value.
+*/
