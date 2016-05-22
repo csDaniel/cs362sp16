c671982cc918f9309e8e8620885a5e850e5f88d9
castigla <castigla@oregonstate.edu>
2016-05-22 17:58:53 -0500

    Assignment 5

---
+++ projects/castigla/dominion/BugsInTeammates.c
@@ -0,0 +1,7 @@
+Bugs for Heidi
+adventurer: test failed. bug fix needed
+smithy: test failed. incorrect number of cards drawn
+
+Bugs for Bryan
+adventurer: test failed. bug fix needed
+smithy: test failed. incorrect number of cards drawn
