1ff1192917468868750a7b876fe0e59109ed0e75
casserras <casserras@gmail.com>
2016-05-22 21:24:34 -0700

    bug 2

--- projects/tangr/BugsInTeammates.c
+++ projects/tangr/BugsInTeammates.c
@@ -3,6 +3,19 @@ John Fodrocy
 ---------------
 
 Smithy Card
-
+////////////////////////////////
 Error - Too many cards in hand
+
+Adventurer Card
+////////////////////////////////
+Error - Score changed 
+Warning - possible error in hand size
+
+Score was not suppose to change
+
 Nick Martin
+--------------------------------------
+
+////////////////////////////////
+
+////////////////////////////////
