b1558d00374c52306c595c3348eee5bb71997121
casserras <casserras@gmail.com>
2016-05-22 23:14:54 -0700

    bug fixes

---
+++ projects/tangr/Bugfixes.c
@@ -0,0 +1,10 @@
+Fixes:
+
+Village Card
+------------------------------
+removal of extra action (3 to 2 actions)
+*note* extra card discard not noted
+
+Adventurer Card
+------------------------------
+Removed action not ever gauranteeing 2 treasure cards
