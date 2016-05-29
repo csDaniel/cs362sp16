1af8623fc1e20d04e55d886ea9b87f115925a50b
Brandon Swanson <swanyriver@gmail.com>
2016-05-29 14:17:24 -0700

    3 partition tests working

--- projects/swansonb/URLValidator/src/UrlValidatorTest.java
+++ projects/swansonb/URLValidator/src/UrlValidatorTest.java
@@ -29,7 +29,7 @@ import junit.framework.TestCase;
  */
 public class UrlValidatorTest extends TestCase {
 
-   private boolean printStatus = false;
+   private boolean printStatus = true;
    private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
 
    public UrlValidatorTest(String testName) {
@@ -37,44 +37,81 @@ public class UrlValidatorTest extends TestCase {
    }
 
    
-   
-   public void testManualTest()
-   {
+   //Testing isValid branch on line 281
+   public void testNullPartition(){
+	   String testString = null;
+	   boolean expectedResult = false;
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	   
-   }
-   
-   public void testYourSecondPartition(){
-	   
-   }
-   
-   
-   public void testIsValid()
-   {
-	   for(int i = 0;i<10000;i++)
-	   {
-		   
+	   boolean result = urlVal.isValid(testString);
+	   if (printStatus) {
+		   System.out.printf(
+			   "PARTITION TEST: isValid(NULL) = %b,  passed:%b\n",
+			   result, result == expectedResult );
 	   }
-   }
-   
-   public void testAnyOtherUnitTest()
-   {
 	   
+	   if (result != expectedResult ) fail();
    }
-   /**
-    * Create set of tests by taking the testUrlXXX arrays and
-    * running through all possible permutations of their combinations.
-    *
-    * @param testObjects Used to create a url.
-    */
    
+   //Testing isValid branch on line 285
+   public void testASCII_PATTERNPartition(){
+	   String testString = "This string will fail the ascii regex";
+	   boolean expectedResult = false;
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   boolean result = urlVal.isValid(testString);
+	   if (printStatus) {
+		   System.out.printf(
+			   "PARTITION TEST: isValid(\"%s\") = %b,  passed:%b\n",
+			   testString, result, result == expectedResult );
+	   }
+	   
+	   if (result != expectedResult ) fail();
+   }
+   
+   //Testing isValid branch on line 291
+   public void testURL_PATTERN_Partition(){
+
+   }
+   
+   //Testing isValid branch on line 295
+   public void testURL_SCHEME_Partition(){
+	  
+   }
+
+   //Testing isValid branch on line 300
+   public void testURL_AUTHORITY_Partition(){
+
+   }
+
+   //Testing isValid branch on line 310
+   public void testURL_PATH_Partition(){
+
+   }
+
+   //Testing isValid branch on line 314
+   public void testURL_QUERY_Partition(){
+
+   }
+
+   //Testing isValid branch on line 318
+   public void testURL_FRAGMENT_Partition(){
+
+   }
+
+   //Testing isValid branch on line 324 (return true)
+   public void testValidPartition(){
+	   String testString = "http://www.google.com";
+	   boolean expectedResult = true;
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   boolean result = urlVal.isValid(testString);
+	   if (printStatus) {
+		   System.out.printf(
+			   "PARTITION TEST: isValid(\"%s\") = %b,  passed:%b\n",
+			   testString, result, result == expectedResult );
+		   System.out.flush();
+	   }
+	   
+	   if (result != expectedResult ) fail();
+   }
+      
 
 }
