2202164cebac5b9b52cba53fb7210f518c2a1386
Michael Stramel <stramelm@flip3.engr.oregonstate.edu>
2016-05-28 19:51:13 -0700

    Added manual tests.

--- projects/stramelm/URLValidator/src/UrlValidatorTest.java
+++ projects/stramelm/URLValidator/src/UrlValidatorTest.java
@@ -40,10 +40,93 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
+	   // manual tests are aimed at achieving 100% code coverage (or close to it)
+	   // using EclEmma 2.3.3 in Eclipse
 	   
+	   // valid and invalid urls tested came from here:
+	   // http://formvalidation.io/validators/uri/
 	   
+	   // these should all be invalid
+	   ManualTests.manualTest076();
+	   ManualTests.manualTest075();
+	   ManualTests.manualTest074();
+	   ManualTests.manualTest073();
+	   ManualTests.manualTest072();
+	   ManualTests.manualTest071();
+	   ManualTests.manualTest070();
+	   ManualTests.manualTest069();
+	   ManualTests.manualTest068();
+	   ManualTests.manualTest067();
+	   ManualTests.manualTest066();
+	   ManualTests.manualTest065();
+	   ManualTests.manualTest064();
+	   ManualTests.manualTest063();
+	   ManualTests.manualTest062();
+	   ManualTests.manualTest061();
+	   ManualTests.manualTest060();
+	   ManualTests.manualTest059();
+	   ManualTests.manualTest058();
+	   ManualTests.manualTest057();
+	   ManualTests.manualTest056();
+	   ManualTests.manualTest055();
+	   ManualTests.manualTest054();
+	   ManualTests.manualTest053();
+	   ManualTests.manualTest052();
+	   ManualTests.manualTest051();
+	   ManualTests.manualTest050();
+	   ManualTests.manualTest049();
+	   ManualTests.manualTest048();
+	   ManualTests.manualTest047();
+	   ManualTests.manualTest046();
+	   ManualTests.manualTest045();
+	   ManualTests.manualTest044();
+	   ManualTests.manualTest043();
+	   ManualTests.manualTest042();
+	   ManualTests.manualTest041();
+	   ManualTests.manualTest040();
+	   ManualTests.manualTest039();
+	   ManualTests.manualTest038();
+	   
+	   // these should all be valid
+	   ManualTests.manualTest037();
+	   ManualTests.manualTest036();
+	   ManualTests.manualTest035();
+	   ManualTests.manualTest034();
+	   ManualTests.manualTest033();
+	   ManualTests.manualTest032();
+	   ManualTests.manualTest031();
+	   ManualTests.manualTest030();
+	   ManualTests.manualTest029();
+	   ManualTests.manualTest028();
+	   ManualTests.manualTest027();
+	   ManualTests.manualTest026();
+	   ManualTests.manualTest025();
+	   ManualTests.manualTest024();
+	   ManualTests.manualTest023();
+	   ManualTests.manualTest022();
+	   ManualTests.manualTest021();
+	   ManualTests.manualTest020();
+	   ManualTests.manualTest019();
+	   ManualTests.manualTest018();
+	   ManualTests.manualTest017();
+	   ManualTests.manualTest016();
+	   ManualTests.manualTest015();
+	   ManualTests.manualTest014();
+	   ManualTests.manualTest013();
+	   ManualTests.manualTest012();
+	   ManualTests.manualTest011();
+	   ManualTests.manualTest010();
+	   ManualTests.manualTest009();
+	   ManualTests.manualTest008();
+	   ManualTests.manualTest007();
+	   ManualTests.manualTest006();
+	   ManualTests.manualTest005();
+	   ManualTests.manualTest004();
+	   ManualTests.manualTest003();
+	   ManualTests.manualTest002();
+	   
+	   // this one should be invalid
+	   ManualTests.manualTest001();
    }
    
    
